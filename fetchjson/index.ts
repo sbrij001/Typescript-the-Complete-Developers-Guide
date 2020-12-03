8cb00cd1b2e97cb5f5aa90f0946432d76e1e2fcc
shivani brijmohan <shivani.brijmohan@gmail.com>
2020-12-02 20:13:43 -0500

    defined an interface within the typescript file

--- fetchjson/index.ts
+++ fetchjson/index.ts
@@ -1,8 +1,24 @@
 import axios from 'axios';
 
-// make a network request to fetch some JSON and print the result. 
+// make a network request to fetch some JSON and print the result.
 
 const url = "https://jsonplaceholder.typicode.com/todos/1";
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
  axios.get(url).then(response => {
-   console.log(response.data);
+   const todo = response.data as Todo;
+   const id = todo.id;
+   const title = todo.title;
+   const finished = todo.completed;
+
+   console.log(`
+    the todo with ID: ${id}
+    had a title of: ${title}
+    is it finished? ${finished}
+   `);
+
  });

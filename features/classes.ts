774d2455d1d3aa6a399b5f9e0cf4101299ac17eb
shivani brijmohan <shivani.brijmohan@gmail.com>
2021-02-24 13:10:07 -0500

    instance methods and modifiers overview

--- features/classes.ts
+++ features/classes.ts
@@ -1,11 +1,21 @@
 class Vehicle{
-  drive(): void{
-    console.log('chugga chugga');
-  }
-
-  honk(): void{
+  protected honk(): void{
     console.log('beep');
-
-  }
+  };
 }
 const vehicle = new Vehicle();
+vehicle.honk();
+
+class Car extends Vehicle{
+  private drive(): void {
+    console.log('vroom');
+  };
+
+  startDrivingProcess(): void{
+    this.drive;
+    this.honk;
+  };
+};
+const car = new Car();
+car.startDrivingProcess();
+car.honk();

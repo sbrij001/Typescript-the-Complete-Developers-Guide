693859314a7a36e1601d4d25330da1881d8e19b1
shivani brijmohan <shivani.brijmohan@gmail.com>
2021-02-24 13:20:16 -0500

    experimenting with fields in classes

--- features/classes.ts
+++ features/classes.ts
@@ -1,21 +1,27 @@
 class Vehicle{
+  constructor(public color:string){}
+
   protected honk(): void{
     console.log('beep');
-  };
+  }
+
 }
-const vehicle = new Vehicle();
-vehicle.honk();
+const vehicle = new Vehicle('yellow');
+console.log(vehicle.color);
 
-class Car extends Vehicle{
-  private drive(): void {
-    console.log('vroom');
-  };
 
-  startDrivingProcess(): void{
-    this.drive;
-    this.honk;
-  };
-};
-const car = new Car();
-car.startDrivingProcess();
-car.honk();
+// class Car extends Vehicle{
+
+//   private drive(): void {
+//     console.log('vroom');
+//   }
+
+//   startDrivingProcess(): void{
+//     this.drive;
+//     this.honk;
+//   }
+
+// };
+
+// const car = new Car();
+// car.startDrivingProcess();

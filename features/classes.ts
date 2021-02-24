273fcb88d232568ae927010a1451fbd9b6808936
shivani brijmohan <shivani.brijmohan@gmail.com>
2021-02-24 13:29:33 -0500

    child classes fields with inheritance overview

--- features/classes.ts
+++ features/classes.ts
@@ -10,18 +10,21 @@ const vehicle = new Vehicle('yellow');
 console.log(vehicle.color);
 
 
-// class Car extends Vehicle{
+class Car extends Vehicle{
+  constructor(public wheels: number, color: string){
+    super(color);
+  }
 
-//   private drive(): void {
-//     console.log('vroom');
-//   }
+  private drive(): void {
+    console.log('vroom');
+  }
 
-//   startDrivingProcess(): void{
-//     this.drive;
-//     this.honk;
-//   }
+  startDrivingProcess(): void{
+    this.drive;
+    this.honk;
+  }
 
-// };
+};
 
-// const car = new Car();
-// car.startDrivingProcess();
+const car = new Car(4,'red');
+car.startDrivingProcess();
